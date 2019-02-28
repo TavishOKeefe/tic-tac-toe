d9d8f7a51eae264d241e35b8bff4161101da7e7f
Michael Brown and Tavish Okeefe <michael+tavish@>
2019-02-27 16:47:49 -0800

    follow react tutorial down to Why Immutability Is Important

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
       </button>
     );
   }
 }
+
+Square.propTypes = {
+  value: PropTypes.number
+};
 
 export default Square;
